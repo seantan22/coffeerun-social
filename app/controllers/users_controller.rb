29316f08a0fda6aef929e79cd16af07944bbebba
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-20 19:09:02 +0000

    Add new user success, redirect and flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  # Create new blank user
+  # New blank user
   def new
     @user = User.new
   end
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save
+      flash[:success] = "Hi " + @user.name + ", welcome to HomeBuddies!"
+      redirect_to @user
     else
       render 'new'
     end
