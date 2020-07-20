dd4efd4024687f606ecd1df42d0135a474f4f749
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-20 19:23:15 +0000

    Added valid signup test

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+    
+    get signup_path
+    
+    # Ensure new user is not created if submitted information is invalid
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {  name: "",
+                                        email: "user@invalid",
+                                        password: "abc123",
+                                        password_confirmation: "123abc" } }
+    end
+    
+    # Ensure failed submission re-renders the new action
+    assert_template 'users/new'
+    
+    # Ensure error messages appear
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    
+  end
+  
+  test "valid signup information" do
+    
+    get signup_path
+    
+    # Ensure new user is not created if submitted information is invalid
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {  name: "Example User",
+                                        email: "user@example.com",
+                                        password: "abc123",
+                                        password_confirmation: "abc123" } }
+    end
+    
+    follow_redirect!
+  
+    # Ensure failed submission re-renders the new action
+    assert_template 'users/show'
+    
+  end
+  
+end
