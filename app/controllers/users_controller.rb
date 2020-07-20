35c60f13c030e7857a37084c2fbf2352532b83d0
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-20 18:43:23 +0000

    Added error handling to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  # Create new user
+  # Create new blank user
   def new
     @user = User.new
   end
@@ -9,5 +9,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  # Create and save new user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
   
 end
