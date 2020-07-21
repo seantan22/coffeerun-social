a95a5a40fd8c48b83f8fc7e1c68beaa990a6da56
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-21 23:19:55 +0000

    Add protection to edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   # New blank user
   def new
@@ -39,6 +41,22 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # Before filters
+    
+    # Confirms a logged in user
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in to access this page."
+        redirect_to login_url
+      end
+    end
+    
+    # Confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
   
 end
