be98a4b87ffcd01ce9538104b78d45c12dffc436
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-21 00:14:46 +0000

    Added remember digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   
+  # Create an accessible attribute
+  attr_accessor :remember_token
+  
   # Alternate Notation: before_save { email.downcase! }
   before_save { self.email = email.downcase }
  
@@ -18,12 +21,29 @@ class User < ApplicationRecord
                     
   validates :password,  presence: true,
                         length: { minimum: 6 }
+                      
                         
   # Returns the hash digest of a given string
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # Returns a random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Remembers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
 end
