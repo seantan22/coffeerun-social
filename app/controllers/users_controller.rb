80c33f4be22c8cc5cb7be590dedbfd3fdcd30e37
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-22 00:47:06 +0000

    Added friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
     # Confirms a logged in user
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in to access this page."
         redirect_to login_url
       end
