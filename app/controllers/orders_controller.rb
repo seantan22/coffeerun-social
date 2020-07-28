2f12961dbfbb153e43a936441bbb384feeea0f97
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-28 03:25:21 +0000

    Update order status

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy, :update]
   
   def create
-    @order = current_user.orders.build(order_params)
+    @order = current_user.orders.build(order_params_new)
     if @order.save
       flash[:success] = "Order created!"
       redirect_to root_url
@@ -12,32 +12,31 @@ class OrdersController < ApplicationController
     end
   end
   
-  
-  
-  
   def edit
     @order = Order.find(params[:id])
   end
   
   def update
     @order = Order.find(params[:id])
-    if @order.update(order_params)
-      flash[:success] = "Status updated"
+    if @order.update(order_params_update)
+      redirect_back(fallback_location: root_path)
+      flash[:success] = "Status updated!"
     else
       render 'edit'
     end
   end
-  
-  
-  
   
   def destroy
   end
   
   private
   
-  def order_params
-    params.require(:order).permit(:item, :details, :vendor, :zone, :size, :status)
+  def order_params_new
+    params.require(:order).permit(:vendor, :item, :size, :details, :zone)
+  end
+  
+  def order_params_update
+    params.permit(:status)
   end
   
 end
